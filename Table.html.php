b7da02a4853529f364f906d39359a1427b57daf7
Hanane <hanane@affilizr.fr>
2015-06-24 10:26:55 +0200

    Update Table.html.php

--- Table.html.php
+++ Table.html.php
@@ -1,2 +1,18 @@
+# Documentation
+
+```JavaScript ```
+```PHP ```
+## Notations conformes aux demandes du responsable développement :
+
+* Préfixes : 
+  * Chaines de caractère  : <code> $strVariable = "mavariable"; </code>
+  * Entiers               : <code> $intVariable = 13;</code>
+  * Booléens              : <code> $Blnresultat = true;  </code>
+  * Fonctions             : <code> function vodMaFonction ();</code>
+  * Objets                : <code> $objMonObjet = new MonObjet();</code>
+  * Tableaux              : <code> $arrTableaux = array()</code>
+  * Ressources            : <code> $resFic = fopen("fichier.txt", 'a')</code>
+  * Formulaire            : <code> form name="formTest"  action="" method="post"  </code>
+  
 
 <span style="color: #f00;">Ciconia/Modules/Administration .. / Gestion de référentiel</span>
